17cf64d9faaf4133a781bf0d5d707adf9e297eee
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-10-08 00:49:41 -0700

    Add colors and background colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    extend: {
+      backgroundColor: {
+        "strong-cyan": "hsl(172, 67%, 45%)",
+        "very-dark-cyan": "hsl(183, 100%, 15%)",
+        "light-grayish-cyan": "hsl(185, 41%, 84%)",
+        "very-light-grayish-cyan": "hsl(189, 41%, 97%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      colors: {
+        "strong-cyan": "hsl(172, 67%, 45%)",
+        "very-dark-cyan": "hsl(183, 100%, 15%)",
+        "dark-grayish-cyan": "hsl(186, 14%, 43%)",
+        "grayish-cyan": "hsl(184, 14%, 56%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
 };
