454e13e747c4cdb792f8ad5cc151b23d614fb22e
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-10-08 00:52:38 -0700

    Add eventlistener to billAmount

--- js/app.js
+++ js/app.js
@@ -8,3 +8,17 @@ const reset = document.querySelector(".reset-btn");
 let billValue = 0;
 let peopleValue = 0;
 let tipValue = 0;
+
+billAmount.addEventListener("input", validateBill);
+function validateBill() {
+  if (billAmount.value.length >= 1) {
+    reset.classList.add("active");
+  }
+  if (billAmount.value.includes(",")) {
+    billAmount.value.replace(",", ".");
+  }
+  billValue = parseFloat(billAmount.value);
+  calculate();
+  console.log(billValue);
+}
+
